023c9580bd61b28168b514d0c8d483c6d7ffb7ff
Hadiid Andri Yulison <hadiidandri2000@gmail.com>
2022-02-04 15:28:11 +0700

    update

--- admin/Dashboard.php
+++ admin/Dashboard.php
@@ -157,15 +157,14 @@
                  </nav>
                  <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
                      <div class="ms-md-auto pe-md-3 d-flex align-items-center">
-                         <div class="input-group">
-                             <span class="input-group-text text-body"><i class="fas fa-search" aria-hidden="true"></i></span>
-                             <input type="text" class="form-control" placeholder="Type here...">
-                         </div>
+                        
                      </div>
                      <ul class="navbar-nav  justify-content-end">
                          <li class="nav-item d-flex align-items-center">
                              <a href="#" class="nav-link text-white font-weight-bold px-0">
-                                 <i class="fa fa-user me-sm-1"></i>
+                                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                                     <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                                 </svg>
                                  <span class="d-sm-inline d-none"><?php echo $_SESSION['username']; ?> |</span>
 
 
@@ -175,7 +174,10 @@
 
                          <li class="nav-item px-3 d-flex align-items-center">
                              <a href="../logout/logout.php" class="nav-link text-white p-0">
-                                 <i class="fa fa-cog"></i>
+                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+                                     <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z" />
+                                     <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
+                                 </svg>
                                  <span class="d-sm-inline d-none">Logout</span>
 
                              </a>
--- admin/suplier.php
+++ admin/suplier.php
@@ -144,7 +144,9 @@
                      <ul class="navbar-nav  justify-content-end">
                          <li class="nav-item d-flex align-items-center">
                              <a href="#" class="nav-link text-white font-weight-bold px-0">
-                                 <i class="fa fa-user me-sm-1"></i>
+                                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                                     <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                                 </svg>
                                  <span class="d-sm-inline d-none"><?php echo $_SESSION['username']; ?> |</span>
 
 
@@ -154,7 +156,10 @@
 
                          <li class="nav-item px-3 d-flex align-items-center">
                              <a href="../logout/logout.php" class="nav-link text-white p-0">
-                                 <i class="fa fa-cog"></i>
+                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+                                     <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z" />
+                                     <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
+                                 </svg>
                                  <span class="d-sm-inline d-none">Logout</span>
 
                              </a>
@@ -171,6 +176,12 @@
              <div class="card mb-4">
                  <div class="card-header pb-0">
                      <h6>Data Supplier</h6>
+                     <div class="col-12 text-end">
+                         <a class="btn bg-gradient-primary mb-0" href="#"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-plus-fill" viewBox="0 0 16 16">
+                                 <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                                 <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z" />
+                             </svg>&nbsp;&nbsp;Tambah Supplier</a>
+                     </div>
                  </div>
                  <div class="card-body px-0 pt-0 pb-2">
                      <div class="table-responsive p-0">
